5c173be0ed85c9853f2cdfb61866b3bd72ade556
cheboi <moscheb96@gmail.com>
2022-12-01 06:48:32 +0300

    authen

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { userRoutes } = require("./Routes/userRoutes");
 
 const app = express();
-dotenv.config()
+dotenv.config();
+app.use(express.json());
+app.use("/user", userRoutes);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is Running on Port : ${process.env.PORT}`);
