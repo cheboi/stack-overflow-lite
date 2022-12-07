b3bb4e8ac0a416392f1cf08057bc20c9de59e8f3
cheboi <moscheb96@gmail.com>
2022-12-07 09:55:43 +0300

    question route

--- questionsAPI/routes/questionRoute.js
+++ questionsAPI/routes/questionRoute.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
 const {
-    getQuestions,
-    updateQuestion,
-    askQuestion,
+  getQuestions,
+  updateQuestion,
+  askQuestion,
+  getQuestionById,
+  deleteQuestion,
 } = require("../controllers/questionController");
 const router = Router();
 
 router.get("/", getQuestions);
 router.post("", askQuestion);
 router.put("/:id", updateQuestion);
+router.get("/:id", verifyToken, getQuestionById);
+router.delete("/:id", verifyToken, deleteQuestion);
 
 module.exports = { router };
