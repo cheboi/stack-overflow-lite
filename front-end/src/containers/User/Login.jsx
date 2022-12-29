5af5afe8523210b1454af90d68f5beb7f8d1a10c
cheboi <moscheb96@gmail.com>
2022-12-29 10:46:32 +0300

    login form styles

--- front-end/src/containers/User/Login.jsx
+++ front-end/src/containers/User/Login.jsx
@@ -1,56 +1,78 @@
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { userLogin } from '../../features/authSlice'
-import { useEffect } from 'react'
-import Error from '../../components/error'
-import Spinner from '../../components/spinner'
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "../../features/authSlice";
+import { useEffect } from "react";
+import Error from "../../components/error";
+import Spinner from "../../components/spinner";
+
+import "./Login.css";
 
 const Login = () => {
-  const { loading, userInfo, error } = useSelector((state) => state.auth)
-  const dispatch = useDispatch()
+  const { loading, userInfo, error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit } = useForm();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // redirect authenticated user to profile screen
   useEffect(() => {
     if (userInfo) {
-      navigate('/')
+      navigate("/");
     }
-  }, [navigate, userInfo])
+  }, [navigate, userInfo]);
 
   const submitForm = (data) => {
-    dispatch(userLogin(data))
-  }
+    dispatch(userLogin(data));
+  };
 
   return (
-    <form onSubmit={handleSubmit(submitForm)} style={{paddingTop: '80px'}}>
+    <form onSubmit={handleSubmit(submitForm)} className="signContinue">
+      <div className="welcome-sign">
+        <h3>Welcome back!</h3>
+        <h5 style={{ color: "#0E4BE8" }}>Signin to continue..</h5>
+      </div>
       {error && <Error>{error}</Error>}
-      <div className='cardcontainer'>
-        <label htmlFor='email'>Email</label>
+      <div className="form-group">
+        <label htmlFor="email">User email</label>
         <input
-          type='email'
-          className='descriptionField'
-          {...register('email')}
+          type="email"
+          className="descriptionField"
+          {...register("email")}
           required
         />
       </div>
-      <div className='form-group'>
-        <label htmlFor='password'>Password</label>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
         <input
-          type='password'
-          className='form-input'
-          {...register('password')}
+          type="password"
+          className="form-input"
+          {...register("password")}
           required
         />
       </div>
-      <button type='submit' className='button' disabled={loading}>
-        {loading ? <Spinner /> : 'Login'}
+      <button type="submit" className="btnPrimary" disabled={loading}>
+        {loading ? <Spinner /> : "Login"}
       </button>
+      <div className="welcome-sign">
+        <h5>You do not have an account?</h5>
+        <Link
+          to="/signup"
+          style={{
+            color: "#1F456E",
+            textDecoration: "non",
+            left: 10,
+            paddingLeft: "300px",
+            fontWeight: "bold",
+            marginBottom: "40px",
+          }}
+        >
+          Sign Up
+        </Link>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
